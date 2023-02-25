ad7a7a96748db4f5f5e34358822c2a11c2950a0e
clagier <charles.lagier@ensae.fr>
2023-02-25 17:44:11 +0000

    Avancement de la séance 2

---
+++ delivery_network/séance2_time_calcul.py
@@ -0,0 +1,38 @@
+import time
+from graph import Graph
+from math import factorial
+data_path = "/home/onyxia/projet_info/input/"
+
+g = Graph.graph_from_file(data_path + file_name)
+
+"""opening each file and calcuting the time """
+for i in range (1,11):
+    file_name = f"routes.{i}.in"
+    g = Graph.graph_from_file(data_path + file_name)
+
+    """there are nb_edges edges, so total number of possible trajects is nb_edges!"""
+    nb_trajet_possible = factorial(nb_edges)
+    print("il y a ", nb_trajet_possible)
+    time_list = ()
+
+    """Calculating the time to find a traject through a sample of trajects """
+    for j in range (100):
+        src=int(rand()*g.nb_edges+1)
+        dest = int(rand()*g.;nb_edges)
+        
+        start_time = time.perf_counter()
+
+        Graph.min_power(g, src, dest)
+
+        end_time = time.perf_counter()
+
+
+        execution_time = end_time - start_time
+        time_list.append(execution_time))
+    
+    execution_time_mean  = mean(time_list)
+    total_time = execution_time_mean*nb_trajects_possible 
+    print("Le temps d'exécution moyen est de :", execution_time_mean, "secondes")
+    print("Le temps total pour calculer l'ensemble des trajets  du graphe""",{i}, "est : """, total_time )
+
+    
