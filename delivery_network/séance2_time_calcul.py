fa5bdccd7350b718b25aa1530e31032bfd657b15
clagier <charles.lagier@ensae.fr>
2023-03-03 22:41:02 +0000

    création de test_s1q1_graph_loading

--- delivery_network/séance2_time_calcul.py
+++ delivery_network/séance2_time_calcul.py
@@ -1,36 +1,41 @@
 import time
 from graph import Graph
-from math import factorial
+import random
+from statistics import mean
+
+
 data_path = "/home/onyxia/projet_info/input/"
 
 """opening each file and calcuting the time """
-for i in range(1,11):
-    file_name = f"routes.{i}.in"
+for i in range(1 ,11):
+    file_name = f"network.{i}.in"
+    route_name = f"routes.{i}.in"
     g = Graph.graph_from_file(data_path + file_name)
 
-    """there are nb_edges edges, so total number of possible trajects is nb_edges!"""
-    nb_trajet_possible = factorial(nb_edges)
-    print("il y a ", nb_trajet_possible)
-    time_list = ()
+    with open(data_path + route_name, 'r') as file: 
+        nb_travel = int(file.readline())
+        print("il y a ", nb_travel, "trajets")
+        content = file.readlines()
+        time_list = list()
 
-    """Calculating the time to find a traject through a sample of trajects """
-    for j in range (100):
-        src=int(rand()*g.nb_edges+1)
-        dest = int(rand()*g.nb_edges)
-        
-        start_time = time.perf_counter()
+        """Calculating the time to find a path through a sample of travel """
+        for j in range(10):
+            nb_ligne = random.randrange(nb_travel) + 1
+            src = content[nb_ligne].split()[0]
+            dest = content[nb_ligne].split()[1]
+                    
+            start_time = time.perf_counter()
 
-        Graph.min_power(g, src, dest)
+            Graph.min_power(g, src, dest)
 
-        end_time = time.perf_counter()
+            end_time = time.perf_counter()
 
-
-        execution_time = end_time - start_time
-        time_list.append(execution_time)
+            execution_time = end_time - start_time
+            time_list.append(execution_time)
     
-    execution_time_mean  = mean(time_list)
-    total_time = execution_time_mean*nb_trajects_possible 
-    print("Le temps d'exécution moyen est de :", execution_time_mean/3600, "heures" )
-    print("Le temps total pour calculer l'ensemble des trajets  du graphe""",{i}, "est : """, total_time/3600, "heures" )
+        execution_time_mean = mean(time_list)
+        total_time = execution_time_mean*nb_travel
+        print("Le temps d'exécution moyen est de :", execution_time_mean/60, "minutes")
+        print("Le temps total pour calculer l'ensemble des trajets  du graphe ", {i}, "est : """, total_time/3600, "heures")
 
     
