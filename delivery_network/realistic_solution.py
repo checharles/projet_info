1b0c8b52c8879e7b59acf3df6af76ded740cd07e
clagier <charles.lagier@ensae.fr>
2023-04-06 19:07:28 +0000

    version finale

--- delivery_network/realistic_solution.py
+++ delivery_network/realistic_solution.py
@@ -2,17 +2,17 @@
 
 
 from graph import Graph, graph_from_file, catalog_from_file
+from statistics import mean
 
 
-data_path = "/home/onyxia/projet_info/input/"
+data_path = "/home/onyxia/projet_info/input/network_importation/"
+truck_path = "/home/onyxia/projet_info/input/truck/"
 
 """the budget, which is a contraint"""
 B = 25*10**9
 
 
-from statistics import mean
 
-data_path = "/home/onyxia/projet_info/input/"
 
 """opening each file and calcuting the time """
 
@@ -42,12 +42,12 @@ def cost_traject_realistic(nb_road, nb_truck):
     """initialisation of the files"""
     file_name = f"network.{nb_road}.in"
     route_name = f"routes.{nb_road}.in"
-    power_road = f"routes_power.{nb_road}.out"
+    edge_road = f"route_edge.{nb_road}.out"
     dist_road = f"route_dist.{nb_road}.out"
     ratio_road = f"route_ratio.{nb_road}.out"
     truck_model = f"trucks.{nb_truck}.in"
 
-    cost_and_power_truck = catalog_from_file(data_path + truck_model)
+    cost_and_power_truck = catalog_from_file(truck_path + truck_model)
 
 
     """initialisation of the different utilities"""
@@ -55,7 +55,8 @@ def cost_traject_realistic(nb_road, nb_truck):
     utility_dist = {}
     utility_ratio = {}
     utility = {}
-    list_travel
+    utility_edge = {}
+    list_travel = list()
     key_travel = 0
 
 
@@ -70,24 +71,27 @@ def cost_traject_realistic(nb_road, nb_truck):
             utility_dist[key_travel] = float(line.split()[2])
             utility_ratio[key_travel] = float(line.split()[2])
             utility[key_travel] = float(line.split()[2])
+            utility_edge[key_travel] = float(line.split()[2])
             src = float(line.split()[0])
             dest = float(line.split()[1])
             travel = (src, dest)
             list_travel.append(travel)
 
 
-    with open(data_path + power_road, 'r') as f:
 
-        """we  read the file with a MST where the weight is the minimun power. The minimun power is found and so the price of the truck. The realistic solition is then found 
-        according the distance the number of edges crossed and the cost of the truck. 
-        """
+    with open(data_path + route_name, 'r') as f:
         
+
+        """we  read the file with a MST where the weight is the distance, which value is always 1. So the path between two nodes has the fewer possible number of edges. 
+        The minimun power is found and so the price 
+        of the truck. The realistic solition is then found according the distance the number of edges crossed and the cost of the truck. 
+        """
+        nb_trajet = int(f.readline())
         content = f.readlines()
         key_travel = 0
         for line in content:
             key_travel = key_travel + 1
             i = 0 
-
             min_power = float(line.split()[0])
             dist = float(line.split()[2])
             lenght_path = int(line.split()[1])
@@ -95,14 +99,13 @@ def cost_traject_realistic(nb_road, nb_truck):
             """The model of the truck is find and its price is calculated"""
             while cost_and_power_truck[i][0] < min_power:
                 i = i+1
+        
             cost_travel = cost_and_power_truck[i][1]
             
-            utility_power[key_travel] = (max(utility_power[key_travel]*((0.001)**lenght_path), 0) - dist*0.01, cost_travel,i + 1)
-
-
+            utility_power[key_travel] = (max(utility_power[key_travel]*((0.999)**lenght_path) - dist*0.01, 0), cost_travel, i + 1, max(utility_edge[key_travel]*((0.999)**lenght_path) - dist*0.01, 0)/cost_travel)
     with open(data_path + dist_road, 'r') as f:
         
-        """we  read the file with a MST where the weight is the distance of the edge. The minimun power is found and so the price of the truck. The realistic solition is 
+        """we  read the file with a MST where the weight is the distance of the edges. The minimun power is found and so the price of the truck. The realistic solition is 
         then found according the distance the number of edges crossed and the cost of the truck. 
         """
         content = f.readlines()
@@ -173,8 +176,8 @@ def cost_traject_realistic(nb_road, nb_truck):
 
     for k in range(1, nb_travel + 1):
 
-        listes = [utility_power[k], utility_dist[k], utility_ratio[k]]
-        utility[k] = max(listes, key=lambda x: x[3])
+        listes = [utility_power[k], utility_dist[k], utility_ratio[k], utility_edge[k]]
+        utility[k] = max(listes, key=lambda x: x[2])
 
         """the nodes connected are added to the possible allocation"""
         utility[k].append(list_travel[k-1])
@@ -230,4 +233,4 @@ def knapsack_greedy_realistic(nb_road, nb_truck, B):
     return total_value, selected_travels, nb_trajets, total_weight
 
 
-print(knappsack_greedy_realistic(1,1,B))
+print(knapsack_greedy_realistic(1,1,B))
